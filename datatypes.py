8ee16f5fed60e983bb7dc1fac8caa562fd57a24c
SamriddhaSahai <komodofrancisi@gmail.com>
2022-07-31 01:36:21 -0700

    basics

---
+++ datatypes.py
@@ -0,0 +1,25 @@
+x = 1920
+y = 8.2
+x2 = -28.5
+y2 = -57.
+print(x, type(x))
+print(y, type(y))
+print(x2, type(x2))
+print(y2, type(y2))
+z = None
+print(z, type(z))
+a = [1,3,6,9,12,15,18] #can be changed
+colors = ['blue', 'red','black']
+print(a, type(a))
+print(colors, type(colors))
+print( x + y)
+#comment :dattebayo
+a = {1,3,5,7,9} #can perform unions and intersections as it is a set
+b = (2,4,6,8,9) #tuples , these are the immutable or unchangeable ones
+car_info = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year" : 1964
+}
+#dictionarydatatypes dictionaries are optimized for retrieving data. we must know the key to retrieve the value.
+print(car_info, type(car_info))
---
+++ operators.py
@@ -0,0 +1,14 @@
+a = 10 
+b = 3
+print(a + b)
+print(a - b)
+print
+print(a * b)
+print(a / b) #float division
+print(a // b) #integer division(quotient)/floor division rounding off
+print(a % b) #remainder
+print(a ** b) #exponent
+fruits = ['Apple','Banana', 'Grapes', 'Oranges','Pear']
+print('Papaya' in fruits)
+print('apple' in fruits)
+print('Apple' in fruits)
